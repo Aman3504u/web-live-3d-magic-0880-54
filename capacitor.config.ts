17e6cef7d6bdc9061bee63bfd940d8fc02002da6
Aman3504u <89192740+Aman3504u@users.noreply.github.com>
2025-09-18 16:32:16 +0530

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.weblive3dmagic',
-  appName: 'web-live-3d-magic',
+  appId: 'app.lovable.77ef1998e786419f844f5e2a97bc1754',
+  appName: 'A Lovable project',
   webDir: 'dist',
   server: {
-    url: "https://2965ecff-fb16-484a-ae5c-6012ea36b0c3.lovableproject.com?forceHideBadge=true",
+    url: 'https://77ef1998-e786-419f-844f-5e2a97bc1754.lovableproject.com?forceHideBadge=true',
     cleartext: true
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: "#1a1a2e",
-      showSpinner: true,
-      spinnerColor: "#6366f1"
-    }
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: true
   }
 };
 
